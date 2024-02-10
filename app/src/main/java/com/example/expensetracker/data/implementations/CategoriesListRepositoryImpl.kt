eb2edb3b9190a81da051bce58bb84cac791e4d67
Savenko Ivan PC <savenkoivan931@gmail.com>
2024-02-10 20:17:35 +0100

    CategoriesListRepositoryImpl now works

--- app/src/main/java/com/example/expensetracker/data/implementations/CategoriesListRepositoryImpl.kt
+++ app/src/main/java/com/example/expensetracker/data/implementations/CategoriesListRepositoryImpl.kt
@@ -7,76 +7,64 @@ import com.example.expensetracker.data.models.ExpenseCategory
 import com.example.expensetracker.domain.repository.CategoriesListRepository
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.withContext
+import kotlin.coroutines.CoroutineContext
 import kotlin.random.Random
 
 class CategoriesListRepositoryImpl(private val categoryDao: ExpenseCategoryDao) : CategoriesListRepository {
-    private var categoriesList = mutableListOf<ExpenseCategory>()
+    private var categoriesList = listOf<ExpenseCategory>()
     private var categoriesNames = categoriesList.map { it.name }
-    override suspend fun setCategoriesList(categoryDao: ExpenseCategoryDao) {
-        withContext(Dispatchers.IO) { categoriesList = categoryDao.getAllCategories().toMutableList() }
+
+    override suspend fun setCategoriesList(categoryDao: ExpenseCategoryDao, context: CoroutineContext) {
+        withContext(context = context) {
+            categoryDao.getAllCategories().collect {
+                categoriesList = it
+            }
+        }
     }
 
-    override fun getCategoriesList(): MutableList<ExpenseCategory> {
+    override fun getCategoriesList(): List<ExpenseCategory> {
         return categoriesList
     }
 
-    override fun getCategoryItem(categoryItemId: Long): ExpenseCategory? {
-        if (categoriesList.find { it.categoryId.toLong() == categoryItemId } == null) {
-            return null
-        } else {
-            return categoriesList.find { it.categoryId.toLong() == categoryItemId } // WARNING !! call, to be checked afterwards
+    override fun getCategoryItem(categoryItemId: Int): ExpenseCategory? {
+        return categoriesList.find { it.categoryId == categoryItemId }
+    }
+
+
+    override suspend fun addCategory(category: ExpenseCategory, context: CoroutineContext) {
+        withContext(context = context) {
+            categoryDao.insert(category)
         }
     }
 
-    override suspend fun editCategory(category: ExpenseCategory) {
-        val olderCategory = getCategoryItem(category.categoryId.toLong())
-        if (olderCategory != null) {
-            categoriesList.remove(olderCategory)
-            addCategory(category)
-            withContext(Dispatchers.IO) {
-                categoryDao.delete(olderCategory)
-                categoryDao.insert(category)
-            }
+    override suspend fun editCategory(category: ExpenseCategory, context: CoroutineContext) {
+        withContext(context = context) {
+            categoryDao.update(category = category)
         }
     }
 
-    override suspend fun addCategory(category: ExpenseCategory) {
-        if (categoriesList.none { it.categoryId == category.categoryId && it.name == category.name }) {
-            categoriesList.add(category)
-            withContext(Dispatchers.IO) {
-                categoryDao.insert(category)
-            }
-        } else {
-            category.categoryId++
-            categoriesList.add(category)
-            withContext(Dispatchers.IO) {
-                categoryDao.insert(category)
-            }
-        }
-    }
 
-    override suspend fun deleteCategory(category: ExpenseCategory) {
-        categoriesList.remove(category)
-        withContext(Dispatchers.IO) {
+    override suspend fun deleteCategory(category: ExpenseCategory, context: CoroutineContext) {
+        withContext(context = context) {
             categoryDao.delete(category)
         }
     }
 
-    override fun checkDefaultCategories(context: Context): Boolean {
+    override suspend fun addDefaultCategories(context: Context, coroutineContext: CoroutineContext) {
         val categoriesNamesFromResources = context.resources.getStringArray(R.array.default_expenses)
-        return categoriesNames.toSet() == categoriesNamesFromResources
-    }
-
-    override suspend fun addDefaultCategories(context: Context) {
         if (!checkDefaultCategories(context)) {
-            val categoriesNamesFromResources = context.resources.getStringArray(R.array.default_expenses)
             categoriesNames = categoriesNames.plus(categoriesNamesFromResources)
-            categoriesNames = categoriesNames.toSet().toList()
+            categoriesNames = categoriesNames.toSet().distinct()
             categoriesNames.forEach { it ->
                 withContext(Dispatchers.IO) {
                     addCategory(ExpenseCategory(name = it, colorId = Random.nextLong(0, Long.MAX_VALUE)))
                 }
             }
         }
+    }
+
+    override fun checkDefaultCategories(context: Context): Boolean {
+        val categoriesNamesFromResources = context.resources.getStringArray(R.array.default_expenses)
+        return categoriesNames.toSet() == categoriesNamesFromResources
     }
 }
