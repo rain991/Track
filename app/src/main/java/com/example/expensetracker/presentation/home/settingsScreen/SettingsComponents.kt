596feab55a00cec1f0d907c9b3f7d17cd697c32e
Savenko Ivan PC <savenkoivan931@gmail.com>
2024-02-28 21:12:39 +0100

    fixed swaping cards in MainScreenFeedViewModel, started implementing settingsScreen, changes in SettingsExpenseScreen

--- app/src/main/java/com/example/expensetracker/presentation/home/settingsScreen/SettingsComponents.kt
+++ app/src/main/java/com/example/expensetracker/presentation/home/settingsScreen/SettingsComponents.kt
@@ -2,6 +2,7 @@ package com.example.expensetracker.presentation.home.settingsScreen
 
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Card
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.runtime.Composable
@@ -16,26 +17,52 @@ import com.example.expensetracker.data.DataStoreManager
 import com.example.expensetracker.data.constants.NAME_DEFAULT
 
 @Composable
-fun Header(textModifier: Modifier, dataStoreManager: DataStoreManager) {
+fun SettingsHeader(textModifier: Modifier, dataStoreManager: DataStoreManager) {
     val currentUserName = dataStoreManager.nameFlow.collectAsState(initial = NAME_DEFAULT)
     Text(
         text = stringResource(R.string.greetings_settings_screen, currentUserName.value),
         modifier = textModifier,
-        style = MaterialTheme.typography.titleMedium.copy(fontSize = 40.sp)
+        style = MaterialTheme.typography.titleMedium.copy(fontSize = 40.sp, color = MaterialTheme.colorScheme.onPrimaryContainer)
     )
     Spacer(modifier = Modifier.height(20.dp))
 }
 
 @Composable
-fun UserPreference(cardModifier: Modifier, dataStoreManager: DataStoreManager) {
+fun UserPreferenceCard(cardModifier: Modifier, dataStoreManager: DataStoreManager) {
     Card(modifier = cardModifier) {
-        Text(text = stringResource(R.string.your_preferences_settings_screen), style = MaterialTheme.typography.titleMedium)
+        Text(
+            text = stringResource(R.string.your_preferences_settings_screen),
+            style = MaterialTheme.typography.titleMedium,
+            color = MaterialTheme.colorScheme.onPrimaryContainer,
+            modifier = Modifier.padding(start = 4.dp)
+        )
         Spacer(modifier = Modifier.height(12.dp))
-        Text("Main currency:")
+        Text(
+            stringResource(R.string.preferable_currency_settings_screen),
+            style = MaterialTheme.typography.bodyMedium.copy(color = MaterialTheme.colorScheme.onPrimaryContainer),
+            modifier = Modifier.padding(start = 4.dp)
+        )
     }
 }
 
 @Composable
-fun ThemePreferences() {
+fun ThemePreferences(cardModifier: Modifier, dataStoreManager: DataStoreManager) {
+    Card(modifier = cardModifier) {
+        Text(
+            text = stringResource(R.string.theme_settings_screen),
+            style = MaterialTheme.typography.titleMedium,
+            color = MaterialTheme.colorScheme.onPrimaryContainer,
+            modifier = Modifier.padding(start = 4.dp)
+        )
+        Spacer(modifier = Modifier.height(12.dp))
+        Text(
+            stringResource(R.string.use_device_theme_settings_screen),
+            style = MaterialTheme.typography.bodyMedium.copy(color = MaterialTheme.colorScheme.onPrimaryContainer),
+            modifier = Modifier.padding(start = 4.dp)
+        )
+    }
+}
+@Composable
+private fun settingsSwitch(){
 
 }
--- app/src/main/java/com/example/expensetracker/presentation/navigation/Screens/SettingsExpenseScreen.kt
+++ app/src/main/java/com/example/expensetracker/presentation/navigation/Screens/SettingsExpenseScreen.kt
@@ -1,15 +1,26 @@
 package com.example.expensetracker.presentation.navigation.Screens
 
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.wrapContentHeight
+import androidx.compose.foundation.layout.wrapContentSize
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.verticalScroll
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.unit.dp
 import com.example.expensetracker.R
 import com.example.expensetracker.data.DataStoreManager
 import com.example.expensetracker.data.viewmodels.common.BottomSheetViewModel
 import com.example.expensetracker.presentation.bottomsheets.ExtendedButtonExample
 import com.example.expensetracker.presentation.bottomsheets.SimplifiedBottomSheet
+import com.example.expensetracker.presentation.home.settingsScreen.SettingsHeader
+import com.example.expensetracker.presentation.home.settingsScreen.ThemePreferences
+import com.example.expensetracker.presentation.home.settingsScreen.UserPreferenceCard
 import com.example.expensetracker.presentation.other.Header
 import org.koin.androidx.compose.koinViewModel
 import org.koin.compose.koinInject
@@ -29,10 +40,28 @@ fun SettingsExpenseScreen() {
         Column(
             modifier = Modifier
                 .padding(it)
+                .verticalScroll(rememberScrollState())
         ) {
-
+            SettingsHeader(
+                textModifier = Modifier
+                    .padding(start = 12.dp)
+                    .wrapContentSize(), dataStoreManager = settingsData
+            )
+            Spacer(modifier = Modifier.height(16.dp))
+            UserPreferenceCard(
+                cardModifier = Modifier
+                    .fillMaxWidth()
+                    .padding(horizontal = 12.dp)
+                    .wrapContentHeight(), dataStoreManager = settingsData
+            )
+            Spacer(modifier = Modifier.height(20.dp))
+            ThemePreferences(
+                cardModifier = Modifier
+                    .fillMaxWidth()
+                    .padding(horizontal = 12.dp)
+                    .wrapContentHeight(), dataStoreManager = settingsData
+            )
         }
         SimplifiedBottomSheet(dataStoreManager = settingsData)
     }
-
 }
