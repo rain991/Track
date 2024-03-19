220ff12e0627ac4a49b1d866e22fe9e057b9ffc7
Savenko Ivan PC <savenkoivan931@gmail.com>
2024-03-19 21:13:19 +0100

    changed chart library (was not yet implemented)

--- app/src/main/java/com/example/track/TrackActivity.kt
+++ app/src/main/java/com/example/track/TrackActivity.kt
@@ -4,6 +4,8 @@ package com.example.track
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.remember
 import androidx.work.Constraints
 import androidx.work.ExistingPeriodicWorkPolicy
 import androidx.work.NetworkType
@@ -12,8 +14,16 @@ import androidx.work.WorkManager
 import androidx.work.workDataOf
 import com.example.track.data.DataStoreManager
 import com.example.track.data.workers.CurrenciesRatesWorker
-import com.example.track.presentation.navigation.Navigation
 import com.example.track.presentation.themes.BlueTheme.BlueTheme
+import com.patrykandpatrick.vico.compose.axis.horizontal.rememberBottomAxis
+import com.patrykandpatrick.vico.compose.axis.vertical.rememberStartAxis
+import com.patrykandpatrick.vico.compose.chart.CartesianChartHost
+import com.patrykandpatrick.vico.compose.chart.layer.rememberLineCartesianLayer
+import com.patrykandpatrick.vico.compose.chart.rememberCartesianChart
+import com.patrykandpatrick.vico.compose.m3.theme.rememberM3VicoTheme
+import com.patrykandpatrick.vico.compose.theme.ProvideVicoTheme
+import com.patrykandpatrick.vico.core.model.CartesianChartModelProducer
+import com.patrykandpatrick.vico.core.model.lineSeries
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.flow.first
@@ -39,8 +49,22 @@ class TrackActivity : ComponentActivity() {
             .enqueueUniquePeriodicWork("currenciesRateRequest", ExistingPeriodicWorkPolicy.KEEP, workRequest)
         setContent {
             BlueTheme {
-                Navigation(dataStore)
+              //  Navigation(dataStore)
+                val modelProducer = remember { CartesianChartModelProducer.build() }
+                LaunchedEffect(Unit) { modelProducer.tryRunTransaction { lineSeries { series(4, 12, 8, 16) } } }
+                ProvideVicoTheme(theme = rememberM3VicoTheme()) {
+                    CartesianChartHost(
+                        rememberCartesianChart(
+                            rememberLineCartesianLayer(),
+                            startAxis = rememberStartAxis(),
+                            bottomAxis = rememberBottomAxis(),
+                        ),
+                        modelProducer,
+                    )
+                }
             }
+
+
         }
     }
 }
