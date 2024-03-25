a681f85f11be2434ecb35f49f25c043fa67dd60b
Savenko Ivan PC <savenkoivan931@gmail.com>
2024-03-25 22:10:23 +0100

    implementation of first and second slot of statistics screen

--- app/src/main/java/com/example/track/presentation/charts/StatisticsSecondSlot.kt
+++ app/src/main/java/com/example/track/presentation/charts/StatisticsSecondSlot.kt
@@ -1,8 +1,33 @@
 package com.example.track.presentation.charts
 
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxHeight
+import androidx.compose.foundation.layout.height
+import androidx.compose.material3.Card
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.unit.dp
 
 @Composable
-fun StatisticsSecondSlot() {
-
+fun StatisticsSecondSlot(modifier: Modifier, isSplited: Boolean, text1: String, text2: String?, textStyle: TextStyle) {
+    if (isSplited && text2 != null) {
+        Box(modifier = modifier) {
+            Card(modifier = Modifier.fillMaxHeight(0.5f)) {
+                Text(text = text1, style =textStyle)
+            }
+            Spacer(modifier = Modifier.height(8.dp))
+            Card(modifier = Modifier.fillMaxHeight(0.5f)) {
+                Text(text = text2, style =textStyle)
+            }
+        }
+    }else{
+        Box(modifier = modifier) {
+            Card(modifier = Modifier.fillMaxHeight()) {
+                Text(text = text1, style =textStyle)
+            }
+        }
+    }
 }
