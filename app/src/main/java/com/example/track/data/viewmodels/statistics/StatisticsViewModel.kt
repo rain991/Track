a4a441b59da66b110197006f754b26d6d893dda8
Savenko Ivan PC <savenkoivan931@gmail.com>
2024-03-30 16:42:32 +0100

    fourthSlot impl

--- app/src/main/java/com/example/track/data/viewmodels/statistics/StatisticsViewModel.kt
+++ app/src/main/java/com/example/track/data/viewmodels/statistics/StatisticsViewModel.kt
@@ -11,11 +11,15 @@ import com.example.track.data.converters.getStartOfMonthDate
 import com.example.track.data.converters.getStartOfYearDate
 import com.example.track.data.implementations.charts.ChartsRepositoryImpl
 import com.example.track.data.implementations.notes.NotesRepositoryImpl
+import com.example.track.data.models.Expenses.ExpenseCategory
+import com.example.track.data.models.incomes.IncomeCategory
+import com.example.track.data.models.other.CategoryEntity
 import com.example.track.presentation.states.screenRelated.StatisticsScreenState
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.asStateFlow
 import kotlinx.coroutines.launch
 import java.time.LocalDate
+import kotlin.random.Random
 
 class StatisticsViewModel(
     private val chartsRepositoryImpl: ChartsRepositoryImpl,
@@ -39,7 +43,6 @@ class StatisticsViewModel(
     private suspend fun requestDataForFirstSlotScreenState() {
         val requestedMapOfResults = mutableMapOf<String, Number?>()
         requestedMapOfResults["requestBiggestExpenseMonthly"] = notesRepositoryImpl.requestBiggestExpenseMonthly()
-        requestedMapOfResults["requestBiggestIncomeMonthly"] = (notesRepositoryImpl.requestBiggestIncomeMonthly())
         requestedMapOfResults["requestBiggestExpenseAnualy"] = (notesRepositoryImpl.requestBiggestExpenseAnualy())
         requestedMapOfResults["requestBiggestIncomeAnualy"] = (notesRepositoryImpl.requestBiggestIncomeAnualy())
         requestedMapOfResults["requestLoginCounts"] = (notesRepositoryImpl.requestLoginCounts())
@@ -54,8 +57,7 @@ class StatisticsViewModel(
 
     private fun requestDataForSecondSlotScreenState() {
         val requestedMapOfResults = mutableMapOf<String, Number?>()
-        requestedMapOfResults["requestSumOfExpensesMonthly"] = notesRepositoryImpl.requestSumOfExpensesMonthly()
-        requestedMapOfResults["requestSumOfIncomesMonthly"] = (notesRepositoryImpl.requestSumOfIncomesMonthly())
+        requestedMapOfResults["requestBiggestIncomeMonthly"] = (notesRepositoryImpl.requestBiggestIncomeMonthly())
         requestedMapOfResults["requestCountOfExpensesMonthly"] = (notesRepositoryImpl.requestCountOfExpensesMonthly())
         requestedMapOfResults["requestCountOfIncomesMonthly"] = (notesRepositoryImpl.requestCountOfIncomesMonthly())
         requestedMapOfResults["requestCountOfExpensesWeekly"] = (notesRepositoryImpl.requestCountOfExpensesWeekly())
@@ -81,31 +83,50 @@ class StatisticsViewModel(
         }
     }
 
-    private fun requestDataForFourthSlotScreenState() {
+    private suspend fun requestDataForFourthSlotScreenState() {
         val requestedMapOfResults = mutableMapOf<String, Number?>()
         requestedMapOfResults["requestSumOfExpensesMonthly"] = notesRepositoryImpl.requestSumOfExpensesMonthly()
-        requestedMapOfResults["requestSumOfIncomesMonthly"] = (notesRepositoryImpl.requestSumOfIncomesMonthly())
-        requestedMapOfResults["requestCountOfExpensesMonthly"] = (notesRepositoryImpl.requestCountOfExpensesMonthly())
-        requestedMapOfResults["requestCountOfIncomesMonthly"] = (notesRepositoryImpl.requestCountOfIncomesMonthly())
-        requestedMapOfResults["requestCountOfExpensesWeekly"] = (notesRepositoryImpl.requestCountOfExpensesWeekly())
-        requestedMapOfResults["requestCountOfIncomesWeekly"] = (notesRepositoryImpl.requestCountOfIncomesWeekly())
-        requestedMapOfResults["requestCountOfExpensesAnualy"] = (notesRepositoryImpl.requestCountOfExpensesAnualy())
-        requestedMapOfResults["requestCountOfIncomeAnualy"] = (notesRepositoryImpl.requestCountOfIncomeAnualy())
+        requestedMapOfResults["requestSumOfIncomesMonthly"] = notesRepositoryImpl.requestSumOfIncomesMonthly()
+        val currentMonthExpenseDistribution = chartsRepositoryImpl.requestCurrentMonthExpenseCategoriesDistribution()
+        val currentMonthIncomeDistribution = chartsRepositoryImpl.requestCurrentMonthIncomeCategoriesDistribution()
+        val sortedExpenseDistribution = currentMonthExpenseDistribution.toList().sortedByDescending { it.second }.toMap()
+        val sortedIncomeDistribution = currentMonthIncomeDistribution.toList().sortedByDescending { it.second }.toMap()
         val filteredResMap = requestedMapOfResults.filterValues { it != null && it.toFloat() > 0f }
         val filteredResMapSize = filteredResMap.size
+
+
         if (filteredResMapSize == 0) return
         if (filteredResMapSize == 1) {
-            val resultValue = filteredResMap.entries.elementAt(0)
-            setSecondSlotMainMessage(getMessageForCurrentSlotEntry(resultValue.key, resultValue.value!!))
+            if (Random.nextBoolean()) {
+                val resultValue = filteredResMap.entries.elementAt(0)
+                setFourthSlotMainMessage(getMessageForCurrentSlotEntry(resultValue.key, resultValue.value!!))
+            } else {
+                if (Random.nextBoolean()) {
+                    setFourthSlotMainMessage(getMessageForCurrentCategoriesDistribution(sortedExpenseDistribution))
+                } else {
+                    setFourthSlotMainMessage(getMessageForCurrentCategoriesDistribution(sortedIncomeDistribution))
+                }
+            }
             return
         } else {
-            val randomIndex = (0..filteredResMapSize).shuffled().first() - 1
-            val mainMessageResult = filteredResMap.entries.elementAt(randomIndex)
-            setSecondSlotMainMessage(getMessageForCurrentSlotEntry(mainMessageResult.key, mainMessageResult.value!!))
-            val additionalMap = filteredResMap.filter { it != mainMessageResult }
-            val additionalRandomIndex = (0..additionalMap.size).shuffled().first() - 1
-            val additionalMessageResult = filteredResMap.entries.elementAt(additionalRandomIndex)
-            setSecondSlotAdditionalMessage(getMessageForCurrentSlotEntry(additionalMessageResult.key, additionalMessageResult.value!!))
+            if (Random.nextBoolean()) {
+                val resultValue = filteredResMap.entries.elementAt(0)
+                setFourthSlotMainMessage(getMessageForCurrentSlotEntry(resultValue.key, resultValue.value!!))
+                if (Random.nextBoolean()) {
+                    setFourthSlotAdditionalMessage(getMessageForCurrentCategoriesDistribution(sortedExpenseDistribution))
+                } else {
+                    setFourthSlotAdditionalMessage(getMessageForCurrentCategoriesDistribution(sortedIncomeDistribution))
+                }
+            } else {
+
+                if (Random.nextBoolean()) {
+                    setFourthSlotMainMessage(getMessageForCurrentCategoriesDistribution(sortedExpenseDistribution))
+                } else {
+                    setFourthSlotMainMessage(getMessageForCurrentCategoriesDistribution(sortedIncomeDistribution))
+                }
+                val resultValue = filteredResMap.entries.elementAt(0)
+                setFourthSlotAdditionalMessage(getMessageForCurrentSlotEntry(resultValue.key, resultValue.value!!))
+            }
             return
         }
     }
@@ -119,40 +140,40 @@ class StatisticsViewModel(
     }
 
     private fun getMessageForCurrentSlotEntry(resultValueKey: String, value: Number): String {
-       return when(resultValueKey) {
+        return when (resultValueKey) {
             // First Slot
-            "requestBiggestExpenseAnualy" ->  ("Biggest expense this year is $value")
-            "requestBiggestIncomeAnualy" ->  ("Biggest income this year is $value ")
-            "requestLoginCounts" ->  ("You have opened app $value times")
-            "requestIdeasCount" ->  ("You have already created $value ideas")
-           "requestBiggestExpenseMonthly" -> ("Biggest expense this month is $value")
-
+            "requestBiggestExpenseAnualy" -> ("Biggest expense this year is $value")
+            "requestBiggestIncomeAnualy" -> ("Biggest income this year is $value ")
+            "requestLoginCounts" -> ("You have opened app $value times")
+            "requestIdeasCount" -> ("You have already created $value ideas")
+            "requestBiggestExpenseMonthly" -> ("Biggest expense this month is $value")
             //Second slot
-           "requestBiggestIncomeMonthly" ->  ("Biggest income this month is $value")
-            "requestCountOfExpensesMonthly" ->  ("You did $value expenses in current month")
-            "requestCountOfIncomesMonthly" ->  ("You did $value incomes in current month")
-            "requestCountOfExpensesWeekly" ->  ("You did $value expenses this week")
-            "requestCountOfIncomesWeekly" ->  ("You did $value incomes this week")
-            "requestCountOfExpensesAnualy" ->  ("In this year you made $value expenses")
-            "requestCountOfIncomeAnualy" ->  ("In this year you made $value incomes")
-           //Third Slot
+            "requestBiggestIncomeMonthly" -> ("Biggest income this month is $value")
+            "requestCountOfExpensesMonthly" -> ("You did $value expenses in current month")
+            "requestCountOfIncomesMonthly" -> ("You did $value incomes in current month")
+            "requestCountOfExpensesWeekly" -> ("You did $value expenses this week")
+            "requestCountOfIncomesWeekly" -> ("You did $value incomes this week")
+            "requestCountOfExpensesAnualy" -> ("In this year you made $value expenses")
+            "requestCountOfIncomeAnualy" -> ("In this year you made $value incomes")
+            //Fourth slot
+            "requestSumOfExpensesMonthly" -> ("Your current month sum of expense is $value")
+            "requestSumOfIncomesMonthly" -> ("Your current month sum of incomes is $value")
+            else -> {
+                ""
+            }
+        }
+    }
 
-
-
-           //Fourth slot
-           "requestSumOfExpensesMonthly" ->  ("Your current month sum of expense is $value")
-           "requestSumOfIncomesMonthly" ->  ("Your current month sum of incomes is $value")
-
-
-
-//           suspend fun requestCurrentMonthExpensesDesc(): List<ExpenseItem>
-//           suspend fun requestCurrentMonthIncomesDesc(): List<IncomeItem>
-//           suspend fun requestCurrentYearExpensesDesc(): List<ExpenseItem>
-//           suspend fun requestCurrentYearIncomesDesc(): List<IncomeItem>
-//           suspend fun requestCurrentMonthExpenseCategoriesDistribution(): Map<ExpenseCategory, Int>
-//           suspend fun requestCurrentMonthIncomeCategoriesDistribution(): Map<IncomeCategory, Int>
-           else -> {""}
-       }
+    private fun getMessageForCurrentCategoriesDistribution(sortedMapDesc: Map<out CategoryEntity, Int>): String {
+        val listOfPairs = sortedMapDesc.toList()
+        val topThree = listOfPairs.take(3)
+        return when (sortedMapDesc.keys.first()) {
+            is ExpenseCategory -> ("Most used expense categories : ${topThree[0].first.note}, ${topThree[1].first.note}, ${topThree[2].first.note}")
+            is IncomeCategory -> ("Most used income categories : ${topThree[0].first.note}, ${topThree[1].first.note}, ${topThree[2].first.note}")
+            else -> {
+                ""
+            }
+        }
     }
 
     private fun getMonthlyChartsAxesLabels(): List<LocalDate> {
@@ -197,7 +218,11 @@ class StatisticsViewModel(
         _statisticsScreenState.value = statisticsScreenState.value.copy(chartData = value)
     }
 
-    private fun setFourthSlotMessage(value: String) {
-        _statisticsScreenState.value = statisticsScreenState.value.copy(fourthSlotMessage = value)
+    private fun setFourthSlotMainMessage(value: String) {
+        _statisticsScreenState.value = statisticsScreenState.value.copy(fourthSlotMainMessage = value)
+    }
+
+    private fun setFourthSlotAdditionalMessage(value: String) {
+        _statisticsScreenState.value = statisticsScreenState.value.copy(fourthSlotAdditionalMessage = value)
     }
 }
