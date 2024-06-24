41bc61aba59fd8c5e6e54ee8f29a97851c45ee4e
Savenko Ivan PC <savenkoivan931@gmail.com>
2024-06-24 16:55:04 +0200

    fixed TrackMainFeedCard layout on extreme screen sizes, version - 3

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -15,7 +15,7 @@ android {
         applicationId = "com.savenko.track"
         minSdk = 26
         targetSdk = 34
-        versionCode = 2
+        versionCode = 3
         versionName = "1.0.2"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
