9aace9120db98ece11475deba8fea7910e954740
Savenko Ivan PC <savenkoivan931@gmail.com>
2024-07-04 20:58:32 +0200

    Track 1.2.1 -> 1.2.2 (version code 12)

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -15,8 +15,8 @@ android {
         applicationId = "com.savenko.track"
         minSdk = 26
         targetSdk = 34
-        versionCode = 11
-        versionName = "1.2.1"
+        versionCode = 12
+        versionName = "1.2.2"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {
