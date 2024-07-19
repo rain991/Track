20fb5ea41984234959b98dccd9ee292783bef72a
IvanSavenko <savenkoivan931@gmail.com>
2024-07-19 22:54:36 +0200

    fixed infinite values in BottomSheetAmountInput

--- app/src/main/java/com/savenko/track/data/other/constants/EntitiesDefaults.kt
+++ app/src/main/java/com/savenko/track/data/other/constants/EntitiesDefaults.kt
@@ -12,13 +12,15 @@ const val SHOW_PAGE_NAME_DEFAULT: Boolean = true
 const val USE_SYSTEM_THEME_DEFAULT: Boolean = true
 val PREFERABLE_THEME_DEFAULT: Themes = Themes.BlueTheme
 const val NON_CATEGORY_FINANCIALS_DEFAULT = false
-const val GROUPING_CATEGORY_ID_DEFAULT = Int.MIN_VALUE // This value used to compare if user selected any category
+const val GROUPING_CATEGORY_ID_DEFAULT =
+    Int.MIN_VALUE // This value used to compare if user selected any category
 const val EXPENSE_CATEGORY_GROUPING_ID_DEFAULT = 13
 const val INCOME_CATEGORY_GROUPING_ID_DEFAULT = 9
 
 
 // Entities defaults
-val CURRENCY_DEFAULT = Currency(ticker = "USD", name = "United States Dollar", CurrencyTypes.FIAT, rate = null)
+val CURRENCY_DEFAULT =
+    Currency(ticker = "USD", name = "United States Dollar", CurrencyTypes.FIAT, rate = null)
 const val DEFAULT_EXPENSE_CATEGORIES_MAX_INDEX = 13
 const val DEFAULT_INCOME_CATEGORIES_MAX_INDEX = 9
 
@@ -29,6 +31,7 @@ const val NAME_MAX_LENGTH = 40
 const val FINANCIAL_NOTE_MAX_LENGTH = 25
 const val MAX_BUDGET_VALUE = Int.MAX_VALUE
 const val MAX_IDEA_VALUE = Int.MAX_VALUE
+const val MAX_FINANCIAL_VALUE = Float.MAX_VALUE
 const val CURRENCIES_RATES_REQUEST_PERIOD = 3L // days
 
 /*  warning changing this parameter will not increase real max value,
