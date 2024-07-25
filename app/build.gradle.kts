f9e035ffd49b704b586b18c0d1162048eac23b2c
Savenko Ivan PC <savenkoivan931@gmail.com>
2024-07-25 14:43:48 +0200

    Track 1.4.0 -> 1.4.1 Improved color contrast in default theme

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -15,8 +15,8 @@ android {
         applicationId = "com.savenko.track"
         minSdk = 26
         targetSdk = 34
-        versionCode = 24
-        versionName = "1.4.0"
+        versionCode = 25
+        versionName = "1.4.1"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {
