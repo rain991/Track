c210a373f5bd548d7baa3bdec443ce793c004e6e
Savenko Ivan PC <savenkoivan931@gmail.com>
2024-08-15 19:09:31 +0200

    Track 1.5.1 -> 1.5.1.1

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -13,8 +13,8 @@ android {
         applicationId = "com.savenko.track"
         minSdk = 26
         targetSdk = 34
-        versionCode = 29
-        versionName = "1.5.1"
+        versionCode = 30
+        versionName = "1.5.1.1"
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {
             useSupportLibrary = true
