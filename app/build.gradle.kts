c41f3f89ee3d020b098c6bf29a5a2a7920f3e112
Savenko Ivan PC <savenkoivan931@gmail.com>
2024-09-03 08:41:32 +0200

    Track 1.6.0 -> 1.6.1 This commit also contains fixes of unused resources

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -20,8 +20,8 @@ android {
         applicationId = "com.savenko.track"
         minSdk = 26
         targetSdk = 34
-        versionCode = 32
-        versionName = "1.6.0"
+        versionCode = 31
+        versionName = "1.6.1"
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {
             useSupportLibrary = true
