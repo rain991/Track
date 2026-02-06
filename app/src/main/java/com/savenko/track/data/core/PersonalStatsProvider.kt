39984552c46e68e5657486942182fd316226e86a
Savenko Ivan <savenkoivan931@gmail.com>
2026-02-06 14:38:45 +0100

    [TW-26] Rework IdeaListRepository.kt and PersonalStatsProvider.kt for correct flow usage

--- app/src/main/java/com/savenko/track/data/core/PersonalStatsProvider.kt
+++ app/src/main/java/com/savenko/track/data/core/PersonalStatsProvider.kt
@@ -14,23 +14,35 @@ class PersonalStatsProvider(
     private val expensesCoreRepositoryImpl: ExpensesCoreRepository,
     private val dataStoreManager: DataStoreManager
 ) {
-    suspend fun provideAllTimeExpensesSum(): Flow<Float> {
-        return expensesCoreRepositoryImpl.getSumOfExpensesInTimeSpan(start = 0, end = System.currentTimeMillis())
+    fun provideAllTimeExpensesSum(): Flow<Float> {
+        return expensesCoreRepositoryImpl.getSumOfExpensesInTimeSpan(
+            start = 0,
+            end = System.currentTimeMillis()
+        )
     }
 
-    suspend fun provideAllTimeIncomesSum(): Flow<Float> {
-        return incomeCoreRepositoryImpl.getSumOfIncomesInTimeSpan(startOfSpan = Date(0), endOfSpan = Date(System.currentTimeMillis()))
+    fun provideAllTimeIncomesSum(): Flow<Float> {
+        return incomeCoreRepositoryImpl.getSumOfIncomesInTimeSpan(
+            startOfSpan = Date(0),
+            endOfSpan = Date(System.currentTimeMillis())
+        )
     }
 
-    suspend fun provideAllTimeIncomesCount(): Flow<Int> {
-        return incomeCoreRepositoryImpl.getCountOfIncomesInSpan(startDate = Date(0), endDate = Date(System.currentTimeMillis()))
+    fun provideAllTimeIncomesCount(): Flow<Int> {
+        return incomeCoreRepositoryImpl.getCountOfIncomesInSpan(
+            startDate = Date(0),
+            endDate = Date(System.currentTimeMillis())
+        )
     }
 
-    suspend fun provideAllTimeExpensesCount(): Flow<Int> {
-        return expensesCoreRepositoryImpl.getCountOfExpensesInSpan(startDate = Date(0), endDate = Date(System.currentTimeMillis()))
+    fun provideAllTimeExpensesCount(): Flow<Int> {
+        return expensesCoreRepositoryImpl.getCountOfExpensesInSpan(
+            startDate = Date(0),
+            endDate = Date(System.currentTimeMillis())
+        )
     }
 
-    suspend fun provideLoginCount(): Flow<Int> {
+    fun provideLoginCount(): Flow<Int> {
         return dataStoreManager.loginCountFlow
     }
 }
